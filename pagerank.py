2c6a5c420567557f132e53355a027be014e18fa3
Ashkon Farhangi <ashkonfarhangi@Ashkons-MacBook-Pro.local>
2015-05-17 00:36:20 -0700

    Added support for nested list matrix representations.

--- pagerank.py
+++ pagerank.py
@@ -1,28 +1,51 @@
 import os
 import sys
-import copy
 import math
 
 import numpy
 import pandas
 
+# Generalized matrix operations:
+
 def __extractNodes(matrix):
     nodes = set()
-    for key1 in matrix:
-        nodes.add(key1)
-        for key2 in matrix[key1]:
-            nodes.add(key2)
+    for colKey in matrix:
+        nodes.add(colKey)
+    for rowKey in matrix.T:
+        nodes.add(rowKey)
     return nodes
 
 def __makeSquare(matrix, keys, default=0.0):
-    matrix = copy.deepcopy(matrix)
-    for key1 in keys:
-        for key2 in keys:
-            if not key1 in matrix:
-                matrix[key1] = {}
-            if not key2 in matrix[key1]:
-                matrix[key1][key2] = 0.0
-    return matrix
+    matrix = matrix.copy()
+    
+    # Add in missing columns:
+    for key in keys:
+        if not key in matrix:
+            matrix[key] = pandas.Series(numpy.zeros(1), index=matrix.index)
+
+    # Add in missing rows:
+    matrix = matrix.T
+    for key in keys:
+        if not key in matrix:
+            matrix[key] = pandas.Series(numpy.zeros(1), index=matrix.index)
+    matrix = matrix.T
+
+    return matrix.fillna(default)
+
+def __ensureRowsPositive(matrix):
+    matrix = matrix.T
+    for colKey in matrix:
+        if matrix[colKey].sum() == 0.0:
+            matrix[colKey] = pandas.Series(numpy.ones(len(matrix[colKey])), index=matrix.index)
+    return matrix.T
+
+def __normalizeRows(matrix):
+    return matrix.div(matrix.sum(axis=1), axis=0)
+
+def __euclideanNorm(series):
+    return math.sqrt(series.dot(series))
+
+# PageRank specific functionality:
 
 def __startState(nodes):
     if len(nodes) == 0: raise ValueError("There must be at least one node.")
@@ -33,17 +56,12 @@ def __integrateRandomSurfer(nodes, transitionProbs, rsp):
     alpha = 1.0 / float(len(nodes)) * rsp
     return transitionProbs.copy().multiply(1.0 - rsp) + alpha
 
-def __normalizeRows(matrix):
-    return matrix.div(matrix.sum(axis=1), axis=0)
-
-def __euclideanNorm(series):
-    return math.sqrt(series.dot(series))
-
 def powerIteration(edgeWeights, rsp=0.15, epsilon=0.00001, maxIterations=1000):
     # Clerical work:
+    edgeWeights = pandas.DataFrame(edgeWeights)
     nodes = __extractNodes(edgeWeights)
-    edgeWeights = __makeSquare(edgeWeights, nodes)
-    edgeWeights = pandas.DataFrame(edgeWeights).T.fillna(0.0)
+    edgeWeights = __makeSquare(edgeWeights, nodes, default=0.0)
+    edgeWeights = __ensureRowsPositive(edgeWeights)
 
     # Setup:
     state = __startState(nodes)
